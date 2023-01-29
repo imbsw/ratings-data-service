db4749f02d2eba521ef764b5cfd049df85eb79f8
imbsw <buddhima.chamaranga@gmail.com>
2023-01-29 12:52:20 +0530

    1 st commit of rating data service

---
+++ src/main/java/io/javabrains/ratingsdataservice/resources/RatingDataResource.java
@@ -0,0 +1,31 @@
+package io.javabrains.ratingsdataservice.resources;
+
+import io.javabrains.ratingsdataservice.models.Rating;
+import io.javabrains.ratingsdataservice.models.UserRating;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingDataResource {
+
+    @RequestMapping("/{movieId}")
+    public Rating getRating(@PathVariable("movieId") String movieId){
+        return new Rating(movieId, 4);
+    }
+
+    @RequestMapping("users/{userId}")
+    public UserRating getUserRating(@PathVariable("userId") String userId){
+        List<Rating> ratings = Arrays.asList(
+                new Rating("500",4),
+                new Rating("550",5)
+        );
+        UserRating userRating = new UserRating();
+        userRating.setUserRating(ratings);
+        return userRating;
+    }
+}
